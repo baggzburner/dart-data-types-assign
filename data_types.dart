86483db17abbeac72e407b307840536b5fd63337
baggzburner <baggzburner@gmail.com>
2024-02-29 16:41:00 +0300

    first commit

---
+++ data_types.dart
@@ -0,0 +1,33 @@
+void main() {
+  // declaring an nteger
+  int num1 = 20;
+  //adding num1+1
+  num1 = num1 + 1;
+  // declaring a double data type
+  double weight = 75.6;
+  weight = weight * 2; //multiplying weight by two
+
+  String firstName = 'brian'; // declaration of a string;
+
+  var languages = [
+    'english',
+    'spanish',
+    'french',
+    'chinese'
+  ]; // declaration of a list
+
+// declaration of a map called cities with country as key and city as value
+  var cities = {
+    'kenya': 'nairobi',
+    'uganda': 'kampala',
+    'nigeria': 'abuja',
+    'somalia': 'mogadishu'
+  };
+
+//printing the variable values of different data types to the console
+  print(num1);
+  print(firstName);
+  print(weight);
+  print(languages);
+  print(cities);
+}
